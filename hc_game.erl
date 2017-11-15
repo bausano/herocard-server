450c87db32471b017215a6d51b619952924e0397
bausano <gtasampserver@seznam.cz>
2017-11-15 11:54:39 +0000

    Game instance draft

---
+++ hc_game.erl
@@ -0,0 +1,19 @@
+%% hc_game provides process for handling a game.
+%%
+%% @author: Michael Bausano
+
+-module(hc_game).
+
+%% Record of game stores a game info.
+-record(game, {
+  %% String id of game under which it's registered.
+  id,
+  %% Current AcceptSocket. Might need revision later on.
+  socket,
+  %% Tuple of two players.
+  players,
+  %% Id of player that is currently on move.
+  move,
+  %% JSON object storing game state.
+  state
+}).
